daffd847ce2b2a36c9096a2546833ad167f361cc
marco-gallegos <ma_galeza@hotmail.com>
2020-11-04 12:21:24 -0600

    [fix] frontend: jquery, popper
    se agregan dependecias jquery y popper necesarias para el funcionamiento
    de bootstrap

--- landing/src/main.js
+++ landing/src/main.js
@@ -8,6 +8,10 @@ import "./assets/css/main.css";
 import "toastr/build/toastr.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+import 'jquery/dist/jquery';
+import "popper.js/dist/popper"
+import "bootstrap/dist/js/bootstrap"
+
 import axios from 'axios';
 import toastr from 'toastr/toastr';
 import moment from 'moment';
@@ -17,5 +21,7 @@ moment.locale('es');
 window.axios = axios;
 window.toastr = toastr;
 window.moment = moment;
+//window.JQuery = jquery;
+//window.$ = jquery;
 
 createApp(App).use(store).use(router).mount('#app')
