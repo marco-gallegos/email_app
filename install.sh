aaa9733bdd15b8e8c5e98436452293b830f91036
marco-gallegos <ma_galeza@hotmail.com>
2020-11-04 23:25:08 -0600

    [imp] install: sleep para migraciones
    la db tarda un poco en iniciarse se deja un sleep de 2 min para ejecutar
    las migraciones

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 docker-compose up -d
+
+echo "espera un tiempo para correr las migraciones de laravel ...."
+sleep 2m
+echo "ejecutando migraciones"
 docker exec turnmyapp_backend_1 php artisan migrate
+echo "si viste un error de migracion ejecuta el comando"
+echo "docker exec turnmyapp_backend_1 php artisan migrate"
