c573a4d4683279a2f60ffe26d2153a03b51fa04d
marco-gallegos <ma_galeza@hotmail.com>
2020-11-04 23:40:33 -0600

    [imp] install uninstall
    se agrega un poco de ux

--- install.sh
+++ install.sh
@@ -2,8 +2,18 @@
 docker-compose up -d
 
 echo "espera un tiempo para correr las migraciones de laravel ...."
-sleep 2m
+
+x=120
+while [ $x -gt 0 ]
+do
+    sleep 1s
+    clear
+    echo "$x esperando .."
+    x=$(( $x -1 ))
+done
+
 echo "ejecutando migraciones"
 docker exec turnmyapp_backend_1 php artisan migrate
 echo "si viste un error de migracion ejecuta el comando"
+
 echo "docker exec turnmyapp_backend_1 php artisan migrate"
